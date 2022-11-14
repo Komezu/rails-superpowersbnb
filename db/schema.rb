a6d2595df319bdd5cfa5acb3fe8f42fca42f3e10
Lisandra Bautista <lsndra.br@gmail.com>
2022-11-14 16:46:45 -0500

    Added data type changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_212711) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_214151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,13 +28,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_212711) do
 
   create_table "superpowers", force: :cascade do |t|
     t.string "name"
-    t.string "description"
+    t.text "description"
     t.string "category"
-    t.string "listed"
     t.integer "price_per_day"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "listed"
     t.index ["user_id"], name: "index_superpowers_on_user_id"
   end
 
