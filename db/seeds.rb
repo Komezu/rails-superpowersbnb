c0410de1f65d9de285352054d88f85038b736775
Lisandra Bautista <lsndra.br@gmail.com>
2022-11-14 17:45:57 -0500

    Created index display for superpowers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Superpower.destroy_all
+User.destroy_all
+
+user = User.create!(email: "first-user@onepunchman.com", password: 123456)
+
+Superpower.create!([{
+  name: "Eat 100 burgers",
+  description: "Want to splurge on burgers but feel too full too fast? Get the ability to able yourself to eat all the burgers you want.",
+  category: "Superhuman",
+  price_per_day: 1200,
+  user:
+},
+{
+  name: "Sleep with eyes open",
+  description: "Tired of being called out in class or your job? Get the ability to sleep with your eyes open!",
+  category: "Superhuman",
+  price_per_day: 5000,
+  user:
+},
+{
+  name: "Electricity Manipulation",
+  description: "Lost electrity in your area? Get the ability to be a hero and light up the whole town.",
+  category: "Elemental",
+  price_per_day: 2800,
+  user:
+},
+{
+  name: "Body Bulk",
+  description: "Got a date and want to impress? Get this ability to bulk up without going to the gym",
+  category: "Transformation",
+  price_per_day: 3000,
+  user:
+},
+{
+  name: "Invisibility",
+  description: "Want to know if your co-workers or friends are talking about you behind your back? Get the ability to become invisible and eavesdrop.",
+  category: "Others",
+  price_per_day: 8000,
+  user:
+}])
+
+p "Created #{Superpower.count} Superpowers"
