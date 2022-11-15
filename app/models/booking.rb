4b2884417120d3085d28c730bbf31fab07e3817d
Lisandra Bautista <lsndra.br@gmail.com>
2022-11-15 15:47:41 -0500

    Added validations, and link to Superpower ID that they can click to view details

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :superpower
 
-  # validates :status, presence: true
-  # validates :start_date, presence: true
-  # validates :end_date, presence: true
+  validates :status, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
