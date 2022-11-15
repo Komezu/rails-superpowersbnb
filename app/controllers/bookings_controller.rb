1782cb119e0dc07ded693a31421df3c1be460e6c
Nifemi Oyekunle <nifemioyekunle@gmail.com>
2022-11-15 16:57:57 -0500

    added form for rentee to book and linked to necessary pages

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,29 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @superpower = Superpower.find(params[:superpower_id])
   end
 
   def create
-    @booking = Booking.new
+    @booking = Booking.new(booking_params)
+    @superpower = Superpower.find(params[:superpower_id])
+    # @booking.user = current_user
+    # @booking.superpower = @superpower
     if @booking.save
       redirect_to_booking_path(@booking)
     else
+      raise
       render :new, status: 422
     end
   end
-  
+
   def index
     @bookings = Booking.all
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :superpowers, only: %i[index show new create]
-  resources :bookings, only: %i[new create]
+  resources :superpowers, only: %i[index show new create] do
+    resources :bookings, only: %i[new create]
+  end
 
   get '/my_superpowers', to: 'superpowers#my_superpowers'
-
-  resources :bookings, only: %w[index show]
+  # resources :bookings, only: %w[index show]
+  # resources :bookings, only: %i[index show]
 end
