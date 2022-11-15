2a0a0a4db8b3d021ffd8079360f826cbbdd2a75d
Jin Qi <jenny_qiqu@hotmail.com>
2022-11-15 13:50:15 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,55 @@
 Superpower.destroy_all
 User.destroy_all
+Booking.destroy_all
 
-user = User.create!(email: "first-user@onepunchman.com", password: 123456)
+user = User.create!(email: "first-user@onepunchman.com", username: "test-user", password: 123456)
 
 Superpower.create!([{
   name: "Eat 100 burgers",
-  description: "Want to splurge on burgers but feel too full too fast? Get the ability to able yourself to eat all the burgers you want.",
-  category: "Superhuman",
+  description: "Want to splurge on burgers but feel too full too fast? Get the ability to eat all the burgers you want.",
+  category: "superhuman",
   price_per_day: 1200,
+  listed: true,
   user:
 },
 {
   name: "Sleep with eyes open",
   description: "Tired of being called out in class or your job? Get the ability to sleep with your eyes open!",
-  category: "Superhuman",
+  category: "imperceptible",
   price_per_day: 5000,
+  listed: true,
   user:
 },
 {
   name: "Electricity Manipulation",
   description: "Lost electrity in your area? Get the ability to be a hero and light up the whole town.",
-  category: "Elemental",
+  category: "elemental",
   price_per_day: 2800,
+  listed: true,
   user:
 },
 {
   name: "Body Bulk",
   description: "Got a date and want to impress? Get this ability to bulk up without going to the gym",
-  category: "Transformation",
+  category: "transformation",
   price_per_day: 3000,
+  listed: true,
   user:
 },
 {
   name: "Invisibility",
   description: "Want to know if your co-workers or friends are talking about you behind your back? Get the ability to become invisible and eavesdrop.",
-  category: "Others",
+  category: "superhuman",
   price_per_day: 8000,
+  listed: false,
+  user:
+},
+{
+  name: "Communicate with animals",
+  description: "Always wanted to know what your pet is complaining about? Get this power to get rid of that language barrier and converse with them!",
+  category: "others",
+  price_per_day: 2000,
+  listed: false,
   user:
 }])
 
